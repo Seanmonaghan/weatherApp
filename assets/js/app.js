d85256b77a93118c4eabfefafb05746f9dcb501d
Sean Monaghan <smonagha@conncoll.edu>
2021-02-25 22:21:38 -0500

    add fetch request function to get data based on City selected

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,14 +2,18 @@ const searchButton = document.getElementById('search-button');
 const searchInput = document.getElementById('search-input');
 
 
+function getApi(location) {
+    console.log(location);
 
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=5d292a57cafb0a0a0714cf2da71abf3d';
 
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
 
-
-
-
-searchButton.addEventListener('click', () => {
-  const inputValue = searchInput.value;
-  alert(inputValue);
-});
-
+getApi('London');
