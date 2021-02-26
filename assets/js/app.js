4d282f5f09a70c361fc1065e02dc609f00cf4e19
Sean Monaghan <smonagha@conncoll.edu>
2021-02-26 16:34:23 -0500

    added full HTML skeleton for javascript to fill with information and getApi function fills them with everything but UVIndex

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,16 @@
 const searchButton = document.getElementById('search-button');
 const searchInput = document.getElementById('search-input');
+const cityInformation = $("#cityInformation");
+const cityInformationTag = $("#cityInformationTag");
+const currentTemperature = $("#currentTemperature");
+const currentHumidity = $("#currentHumidity");
+const currentWindSpeed = $("#currentWindSpeed")
+const currentUVIndex = $("#currentUNIndex")
+const card = $(".card");
 
 
 function getApi(location) {
-    console.log(location);
-
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=5d292a57cafb0a0a0714cf2da71abf3d';
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=imperial&appid=5d292a57cafb0a0a0714cf2da71abf3d';
 
     fetch(requestUrl)
         .then(function (response) {
@@ -13,7 +18,21 @@ function getApi(location) {
         })
         .then(function (data) {
             console.log(data);
-        })
-}
+            console.log(data.weather[0].main);
+            card.css("background-color", "white");
+            card.css("border", "1px solid rgba(0,0,0,.125);")
+            cityInformationTag.text("Current Weather for " + data.name);
+            currentTemperature.text("The current temperature is " + data.main.temp + "°F");
+            currentHumidity.text("The current level of humidity is " + data.main.humidity + "%");
+            currentWindSpeed.text("The current wind speed is " + data.wind.speed + "MPH");
+        });
+    
+};
 
-getApi('London');
+
+
+searchButton.addEventListener('click', () => {
+    const inputValue = searchInput.value;
+    getApi(inputValue);
+
+});
