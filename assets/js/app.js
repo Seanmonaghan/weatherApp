97d51dac7b44d599980b8d5ab9ab7be12403d223
Sean Monaghan <smonagha@conncoll.edu>
2021-02-28 00:38:32 -0500

    Add luxon to get proper dates in the 5 day forecast to display based on timezone of city selected.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,9 @@
+var DateTime = luxon.DateTime;
+
 const searchButton = document.getElementById('search-button');
 const searchInput = document.getElementById('search-input');
 const cityInformation = $("#cityInformation");
 const previousSearchedCitiesList = $("#previousSearchedCitiesList")
-
 
 
 
@@ -11,7 +12,7 @@ const cityInformationTag = $("#cityInformationTag");
 const currentTemperature = $("#currentTemperature");
 const currentHumidity = $("#currentHumidity");
 const currentWindSpeed = $("#currentWindSpeed")
-const currentUVIndex = $("#currentUNIndex")
+const currentUVIndex = $("#currentUVIndex")
 const card = $(".card");
 
 const fiveDayForecastSpan = $("#fiveDayForecastSpan")
@@ -26,10 +27,13 @@ function getApi(location) {
 
     fetch(requestUrl)
         .then(function (response) {
+            if (response.status !== 200) {
+                alert("city not found - please try again!");
+              }
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
+            
             card.css("background-color", "white");
             card.css("border", "1px solid rgba(0,0,0,.125);")
             cityInformationTag.text("Current Weather for " + data.name + ", " +data.sys.country);
@@ -45,19 +49,37 @@ function getApi(location) {
 };
 
 function fiveDayForecast(lat, lon) {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=5d292a57cafb0a0a0714cf2da71abf3d';
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=5d292a57cafb0a0a0714cf2da71abf3d';
 
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
+            console.log(data);
+            
+            var local = DateTime.local();
+            var reZoned = local.setZone(data.timezone);
+            var shortReZoned = reZoned.toLocaleString();
+            var day2 = reZoned.plus({days:1}).toLocaleString();
+            var day3 = reZoned.plus({days:2}).toLocaleString();
+            var day4 = reZoned.plus({days:3}).toLocaleString();
+            var day5 = reZoned.plus({days:4}).toLocaleString();
+
+
+            
+            currentUVIndex.text("The current UVI is " + data.current.uvi);
+
+
+
+            // DO THIS NEXT
             fiveDayForecastSpan.text("Five Day Forecast");
-            firstDayForecastSpan.text("Today:");
-            secondDayForecastSpan.text("Second Day Forecast:");
-            thirdDayForecastSpan.text("Third Day Forecast:");
-            fourthDayForecastSpan.text("Fourth Day Forecast Span:");
-            fifthDayForecastSpan.text("Fifth Day Forecast Span");
+            
+            firstDayForecastSpan.text(shortReZoned);
+            secondDayForecastSpan.text(day2);
+            thirdDayForecastSpan.text(day3);
+            fourthDayForecastSpan.text(day4);
+            fifthDayForecastSpan.text(day5);
             
         });
 };
